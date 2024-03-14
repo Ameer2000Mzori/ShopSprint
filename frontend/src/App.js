f66176cc1ce1538c103434ee0d380afa9694d2c9
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-14 21:17:48 +0100

    added not found page too

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/pages/home/Home.jsx'
+import NotFoundPage from './components/pages/notfoundpage/NotFoundPage.jsx'
 import Navbar from './components/Navbar.js'
 import UtilityBar from './components/pages/home/component/UtilityBar.jsx'
 
@@ -9,6 +10,7 @@ function App() {
       <UtilityBar />
       <Navbar />
       <Routes>
+        <Route path="*" element={<NotFoundPage />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom'
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Prodycts</Link>
+          <Link to="/cart">Cart</Link>
+        </li>
+      </ul>
+    </nav>
+  )
 }
 
 export default Navbar
