28188afb7efdbdc623cbfb68d2a64dfea0f08d74
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-16 15:43:49 +0100

    added backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
 import express from 'express'
 
+const Router = express.Router()
 const app = express()
 app.use(express.json())
-const Router = express.Router()
 app.use(Router)
 
 Router.get('/', (req, res) => {
   res.send('Hello World')
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000')
 })
