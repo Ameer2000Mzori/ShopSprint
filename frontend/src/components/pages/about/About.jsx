5b5b06737f8c3f4334cd385dafaa24079cf57e68
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-16 15:25:50 +0100

    added about page with all content required

--- frontend/src/components/pages/about/About.jsx
+++ frontend/src/components/pages/about/About.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
 const About = () => {
-  return <div>About</div>
+  return (
+    <div className="h-[70vh] flex flex-col text-center items-center justify-center">
+      <h1 className="text-[40px] font-bold text-gray-400 w-[750px]">
+        We Love{' '}
+        <b className="text-white bg-blue-500 p-2 rounded-lg">Sport Shop</b>
+      </h1>
+      <p className="max-w-[350px] text-gray-400 font-bold flex flex-col text-start">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quasi
+        dolores mollitia tempora vitae, fuga libero tenetur dolorem odio
+        adipisci facere, saepe labore ipsum itaque laudantium perferendis fugit
+        ipsam tempore?
+      </p>
+    </div>
+  )
 }
 
 export default About
