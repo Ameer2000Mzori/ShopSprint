a5c73a9baf70c317ff77fe4299b672f652fb6986
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-16 15:41:28 +0100

    added small changes to backend an added some packages an built small port

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+const Router = express.Router()
+app.use(Router)
+
+Router.get('/', (req, res) => {
+  res.send('Hello World')
+})
