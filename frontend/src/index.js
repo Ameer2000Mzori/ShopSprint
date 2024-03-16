0645a04a9a5a8886f85e6937b2b5e6177780e41e
Ameer Ameen <ameerapex496@gmail.com>
2024-03-16 20:54:34 +0100

    wrapped our index file with query client from tanstack

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const queryClient = new QueryClient()
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+)
