403fc71826cbf18f2f9a43873bf5eda434e571e2
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-17 22:17:48 +0100

    added alot of styling and changes

--- frontend/src/components/pages/selectedproduct/SelectedProduct.jsx
+++ frontend/src/components/pages/selectedproduct/SelectedProduct.jsx
@@ -28,26 +28,70 @@ const SelectedProduct = () => {
             alt=""
           />
         </div>
-        <div className="h-[80%] w-[35%] flex flex-col text-center items-center justify-center ga-4">
-          <h1 className="text-[30px] font-bold">{data.item.title}</h1>
-          <div className="w-[100%] h-[1px] rounded-md bg-gray-300"></div>
+        <div className="h-[80%] w-[35%] flex flex-col text-start items-start justify-center ga-4">
+          <h1 className="text-[30px] font-bold text-gray-400">
+            {data.item.title}
+          </h1>
           <div className="w-[100%] h-[500px] flex flex-col items-center justify-center">
-            <h1>for only ${data.item.price}</h1>
-            <h1>{data.item.category}</h1>
-            <h1>from {data.item.company}</h1>
-            <h1> freeShipping ? {data.item.freeShipping ? 'yes' : 'no'}</h1>
-            <h1>category : {data.item.category}</h1>
-            <div className="flex flex-row text-center items-center justify-center w-[250px] h-[100px] gap-3">
-              {data.item.colors.map((color) => {
-                return (
-                  <div
-                    style={{ backgroundColor: `${color}` }}
-                    className="w-[45px] h-[45px] rounded-[100%] flex flex-col items-center justify-center"
-                  >
-                    <h1 className="text-white">{color}</h1>
-                  </div>
-                )
-              })}
+            <div className="flex flex-col text-start items-start justify-start gap-6 w-[100%] h-[100%]">
+              <div className="font-bold text-gray-400 text-[20px] flex flex-row text-start items-start justify-start  gap-4 w-[100%]">
+                <h1>{data.item.category}</h1>|<h1>from {data.item.company}</h1>|
+                <h1>category : {data.item.category}</h1>
+              </div>
+
+              <div className="font-bold text-gray-400  flex flex-row text-start items-start justify-start  gap-4 w-[100%]">
+                <h1 className="text-[15px]"> ${data.item.price}</h1>|
+                <h1 className="text-[15px]">
+                  freeShipping {data.item.freeShipping ? 'yes' : 'no'}
+                </h1>
+              </div>
+
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                placeat pariatur voluptas, mollitia accusantium unde quasi quos
+                ipsam ullam ratione delectus! Tenetur rem magnam nulla,
+                voluptates animi a dolore molestiae?
+              </p>
+
+              <div className=" bg-zinc-300  rounded-md flex flex-row text-center items-center justify-between p-2 w-[100%] h-[50px] ">
+                <div className="flex flex-raw gap-1 w-[25%]">
+                  <p>color : </p>
+                  {data.item.colors.map((color, index) => {
+                    return (
+                      <div
+                        style={{ backgroundColor: color }}
+                        className="rounded-[15%]"
+                      >
+                        <input
+                          id={color}
+                          type="radio"
+                          name="nameOption"
+                          value={color}
+                          key={index}
+                          style={{
+                            backgroundColor: 'transparent',
+                            border: `1px solid ${color}`,
+                          }}
+                          className={`w-[25px] h-[25px] bg-${color} rounded-[100%] flex flex-col items-center justify-center`}
+                        />{' '}
+                      </div>
+                    )
+                  })}
+                </div>
+
+                <select>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                </select>
+              </div>
             </div>
           </div>
         </div>
