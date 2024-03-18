97e6f5a3f8371665da0310899e5b00d49e084b10
Ameer Ameen <ameerapex496@gmail.com>
2024-03-18 02:07:31 +0100

    added more action to the cartSlice file

--- frontend/src/components/features/cart/cartSlice.js
+++ frontend/src/components/features/cart/cartSlice.js
@@ -9,6 +9,12 @@ export const cartSlice = createSlice({
     addToCart: (state, action) => {
       state.items.push(action.payload)
     },
+    removeFromCart: (state, action) => {
+      state.items = state.items.filter((item) => item._id !== action.payload)
+    },
+    clearCart: (state) => {
+      state.items = []
+    },
   },
 })
 
