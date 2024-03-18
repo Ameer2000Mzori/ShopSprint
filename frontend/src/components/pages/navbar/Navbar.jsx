cd4f5761ce5cc306dbc81b1dc641d44f44146997
Ameer Ameen <ameerapex496@gmail.com>
2024-03-18 02:06:45 +0100

    now the user can see how much items he got in the cart

--- frontend/src/components/pages/navbar/Navbar.jsx
+++ frontend/src/components/pages/navbar/Navbar.jsx
@@ -3,8 +3,10 @@ import WebsiteLogo from '../../../assets/36031.webp'
 import NavLinks from './component/NavLinks.jsx'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
-
+import { useSelector } from 'react-redux'
 const Navbar = () => {
+  const items = useSelector((state) => state.cart.items)
+
   return (
     <nav className="w-[100%] h-[70px] flex flex-row text-center items-center justify-center bg-[#f0f6ff]">
       <div className="w-[65%] h-[40px] flex flex-row text-center items-center justify-between">
@@ -17,7 +19,7 @@ const Navbar = () => {
         <div className="w-[20%] text-[25px] flex flex-col text-center items-end justify-center pr-8 ">
           <Link className=" relative w-[40px] h-[100%]" to="/cart">
             <h1 className=" rounded-md w-[25px] h-[15px] bg-blue-600 text-white absolute top-[-5px] left-[20px] text-[10px]">
-              0
+              {items ? items.length : 0}
             </h1>
             <FontAwesomeIcon icon={faShoppingCart} />
           </Link>
