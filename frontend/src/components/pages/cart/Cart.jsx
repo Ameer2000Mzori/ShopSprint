4c129d5c94694e3d5ee9208e27a7b150c52e03f6
Ameer Ameen <ameerapex496@gmail.com>
2024-03-18 02:58:53 +0100

    trying to figure out how to list the items in cart page

--- frontend/src/components/pages/cart/Cart.jsx
+++ frontend/src/components/pages/cart/Cart.jsx
@@ -1,20 +1,27 @@
-import { StyledPageWrapper } from '../../shared/StyledComponents.jsx'
+import {
+  StyledPageWrapper,
+  StyledHeaderTitle,
+  StyledListsStyle,
+} from '../../shared/StyledComponents.jsx'
 import { useSelector } from 'react-redux'
 const Cart = () => {
   const items = useSelector((state) => state.cart.items)
   return (
     <StyledPageWrapper>
       <div>
-        <h1>products in cart</h1>
-        <div>
-          {items.map((item) => {
-            return (
-              <div>
-                <h1>{item.title}</h1>
-                <h1>${item.price}</h1>
-              </div>
-            )
-          })}
+        <StyledHeaderTitle>products in cart</StyledHeaderTitle>
+        <div className="w-[50%] flex flex-col text-center items-center justify-start">
+          <ul className="w-[100%] overflow-auto flex flex-col text-center items-center justify-start">
+            {items.map((item) => {
+              return (
+                <div className="w-[100%]">
+                  <p>{item.title}</p>
+                  <p>${item.price}</p>
+                  <p>quantity: {item.amount}</p>
+                </div>
+              )
+            })}
+          </ul>
         </div>
       </div>
     </StyledPageWrapper>
--- frontend/src/components/shared/StyledComponents.jsx
+++ frontend/src/components/shared/StyledComponents.jsx
@@ -9,3 +9,21 @@ item-align: center;
 gap-4;
 justify-content: center;
 `
+
+export const StyledHeaderTitle = styled.h1`
+  font-size: 40px;
+  font-style: bold;
+  color: #9ca3af;
+`
+
+export const StyledListsStyle = styled.li`
+  font-size: 20px;
+  font-style: bold;
+  width: 100%;
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  gap: 1rem;
+  padding-right: 1rem;
+`
