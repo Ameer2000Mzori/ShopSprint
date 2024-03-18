d89c05692e40089410897f73308d4b34f33cc995
Ameer Ameen <ameerapex496@gmail.com>
2024-03-18 02:21:48 +0100

    small changes

--- frontend/src/components/features/cart/cartSlice.js
+++ frontend/src/components/features/cart/cartSlice.js
@@ -10,7 +10,7 @@ export const cartSlice = createSlice({
       state.items.push(action.payload)
     },
     removeFromCart: (state, action) => {
-      state.items = state.items.filter((item) => item._id !== action.payload)
+      state.items = state.items.filter((item) => item.id !== action.payload.id)
     },
     clearCart: (state) => {
       state.items = []
