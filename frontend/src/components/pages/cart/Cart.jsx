6bec6c3cd62d0e9f300ccd808c3c8d0d71344655
Ameer Ameen <ameerapex496@gmail.com>
2024-03-18 02:27:14 +0100

    added whole logic where we get items from react redux toolkit

--- frontend/src/components/pages/cart/Cart.jsx
+++ frontend/src/components/pages/cart/Cart.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
-
+import { StyledPageWrapper } from '../../shared/StyledComponents.jsx'
+import { useSelector } from 'react-redux'
 const Cart = () => {
-  return <div>Cart</div>
+  const items = useSelector((state) => state.cart.items)
+  return (
+    <StyledPageWrapper>
+      <div>
+        <h1>products in cart</h1>
+        <div>
+          {items.map((item) => {
+            return (
+              <div>
+                <h1>{item.title}</h1>
+                <h1>${item.price}</h1>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </StyledPageWrapper>
+  )
 }
 
 export default Cart
