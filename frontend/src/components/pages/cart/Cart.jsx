697440382089de5d07c85d5984382730955d47b1
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-19 17:33:32 +0100

    added more styling and some more logic

--- frontend/src/components/pages/cart/Cart.jsx
+++ frontend/src/components/pages/cart/Cart.jsx
@@ -1,8 +1,4 @@
-import {
-  StyledPageWrapper,
-  StyledHeaderTitle,
-  // StyledListsStyle,
-} from '../../shared/StyledComponents.jsx'
+import { StyledHeaderTitle } from '../../shared/StyledComponents.jsx'
 import { useSelector, useDispatch } from 'react-redux'
 import { removeFromCart } from '../../features/cart/cartSlice.js'
 const Cart = () => {
@@ -14,15 +10,15 @@ const Cart = () => {
   }
   const items = useSelector((state) => state.cart.items)
   return (
-    <div className="flex flex-col h-[80vh] text-center items-center justify-center">
+    <div className="flex flex-col h-[80vh] text-center items-center justify-center ">
       <StyledHeaderTitle>products in cart</StyledHeaderTitle>
 
-      <div className="h-[90%] w-[90%] flex flex-row items-center justify-center">
+      <div className="h-[90%] w-[90%] flex flex-row items-center justify-center gap-4">
         <div className=" h-[80%] w-[45%] flex flex-col text-center items-center justify-start">
           <ul className="w-[100%]  flex flex-col text-center items-center justify-start">
             {items.map((item) => {
               return (
-                <div className="w-[100%] flex flex-row text-start items-center justify-start pl-2 gap-4 h-[50px] bg-zinc-400 m-1 relative">
+                <div className="w-[100%] flex flex-row text-start items-center justify-start pl-2 gap-4 h-[50px] bg-zinc-300 m-1 relative text-black rounded-lg">
                   <p>{item.name}</p>
                   <p>${item.price}</p>
                   <p>quantity: {item.amount}</p>
@@ -44,7 +40,33 @@ const Cart = () => {
             })}
           </ul>
         </div>
-        <div className="w-[25%] "></div>
+        <div className="w-[25%] h-[80%] flex flex-col text-start items-end justify-start ">
+          <div className="h-[250px] w-[100%] rounded-lg bg-zinc-200">
+            <div>
+              <ul>
+                <li className="h-[40px] w-[100%] flex flex-row text-center items-center justify-between pr-2 pl-2  border-b-2 border-gray-300">
+                  <p>tax</p>
+                  <p>${items.price ? `${items.price * 0.15}` : 0}</p>
+                </li>
+                <li className="h-[40px] w-[100%] flex flex-row text-center items-center justify-between pr-2 pl-2  border-b-2 border-gray-300">
+                  <p>shipping fee</p>
+                  <p>$0</p>
+                </li>
+
+                <li className=" text-[20px] font-bold h-[40px] w-[100%] flex flex-row text-center items-center justify-between pr-2 pl-2  border-b-4 border-gray-300">
+                  <p>total</p>
+                  <p>
+                    $
+                    {items.reduce(
+                      (acc, item) => acc + item.price * item.amount,
+                      0
+                    )}
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
