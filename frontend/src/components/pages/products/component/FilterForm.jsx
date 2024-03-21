872329142f0f3358b5cfc71b1fb5fce73eae4bc6
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-21 18:01:18 +0100

    added styling to the form

--- frontend/src/components/pages/products/component/FilterForm.jsx
+++ frontend/src/components/pages/products/component/FilterForm.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import { StyledInputWrap } from './StyledForm.jsx'
+import {
+  StyledInputWrap,
+  StyledSelectWrap,
+  StyledShippingWrap,
+  StyledCollectionWrap,
+} from './StyledForm.jsx'
 
 const FilterForm = ({
   searchTerm,
@@ -17,103 +22,116 @@ const FilterForm = ({
   filterProducts,
 }) => {
   return (
-    <form className="h-[20vh] flex flex-row flex-wrap text-center items-center justify-center gap-2 ">
-      <StyledInputWrap className=" w-[200px] flex flex-col text-start items-start justify-center">
-        <label htmlFor="searchTerm">search</label>
-        <input
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          type="text"
-          id="searchTerm"
-          name="searchTerm"
-        />
-      </StyledInputWrap>
+    <form className="h-[20vh] flex flex-col  text-center items-center justify-around gap-2  ">
+      <div className="w-[90%] h-[70px] flex flex-row text-center items-center justify-evenly">
+        <StyledSelectWrap>
+          <div className="flex flex-row gap-2">
+            <label htmlFor="price">price</label> <p>{newPrice}</p>
+          </div>
+          <input
+            value={newPrice}
+            onChange={(e) => setNewPrice(e.target.value)}
+            type="range"
+            id="price"
+            name="price"
+            min={15}
+            max={500}
+            step={5}
+          />
+        </StyledSelectWrap>
 
-      <StyledInputWrap>
-        <label htmlFor="price">price</label>
-        <input
-          value={newPrice}
-          onChange={(e) => setNewPrice(e.target.value)}
-          type="text"
-          id="price"
-          name="price"
-        />
-      </StyledInputWrap>
+        <StyledShippingWrap>
+          <label htmlFor="Shipping">free shipping?</label>
+          <input
+            value={newFreeShipping}
+            onChange={() => setNewFreeShipping((prev) => !prev)}
+            type="checkbox"
+            id="Shipping"
+            name="Shipping"
+          />
+        </StyledShippingWrap>
 
-      <StyledInputWrap>
-        <label htmlFor="category">category</label>
-        <select
-          value={newCategory}
-          onChange={(e) => setNewCategory(e.target.value)}
-          id="category"
-          name="category"
+        <StyledSelectWrap>
+          <label htmlFor="category">category</label>
+          <select
+            className="w-[50px] h-[25px]"
+            value={newCategory}
+            onChange={(e) => setNewCategory(e.target.value)}
+            id="category"
+            name="category"
+          >
+            <option value="All">All</option>
+            <option value="Clothing">Clothing</option>
+            <option value="Accessories">Accessories</option>
+            <option value="Footwear">Footwear</option>
+          </select>
+        </StyledSelectWrap>
+
+        <StyledSelectWrap>
+          <label htmlFor="company">company</label>
+          <select
+            className="w-[50px] h-[25px]"
+            value={newCompany}
+            onChange={(e) => setNewCompany(e.target.value)}
+            id="company"
+            name="company"
+          >
+            <option value="All">All</option>
+            <option value="Company A">Company A</option>
+            <option value="Company B">Company B</option>
+            <option value="Company C">Company C</option>
+            <option value="Company D">Company D</option>
+          </select>
+        </StyledSelectWrap>
+
+        <StyledSelectWrap>
+          <label htmlFor="typeOfSorting">Sorting</label>
+          <select
+            className="w-[50px] h-[25px]"
+            value={newSorting}
+            onChange={(e) => setNewSorting(e.target.value)}
+            id="typeOfSorting"
+            name="typeOfSorting"
+          >
+            <option value="a-z">A-Z</option>
+            <option value="z-a">Z-A</option>
+            <option value="low-high">Low to High</option>
+            <option value="high-low">High to Low</option>
+          </select>
+        </StyledSelectWrap>
+      </div>
+
+      <div className="w-[90%] h-[70px] flex flex-row text-center items-center justify-evenly">
+        <button
+          className=" w-[150px] h-[40px] bg-zinc-400 text-white rounded-lg text-[18px]"
+          onClick={(e) => {
+            e.preventDefault()
+
+            filterProducts(
+              searchTerm,
+              newPrice,
+              newCategory,
+              newCompany,
+              newSorting,
+              newFreeShipping
+            )
+          }}
         >
-          <option value="All">All</option>
-          <option value="Clothing">Clothing</option>
-          <option value="Accessories">Accessories</option>
-          <option value="Footwear">Footwear</option>
-        </select>
-      </StyledInputWrap>
+          search
+        </button>
 
-      <StyledInputWrap>
-        <label htmlFor="company">company</label>
-        <select
-          value={newCompany}
-          onChange={(e) => setNewCompany(e.target.value)}
-          id="company"
-          name="company"
-        >
-          <option value="All">All</option>
-          <option value="Company A">Company A</option>
-          <option value="Company B">Company B</option>
-          <option value="Company C">Company C</option>
-          <option value="Company D">Company D</option>
-        </select>
-      </StyledInputWrap>
-
-      <StyledInputWrap>
-        <label htmlFor="typeOfSorting">Type Of Sorting</label>
-        <select
-          value={newSorting}
-          onChange={(e) => setNewSorting(e.target.value)}
-          id="typeOfSorting"
-          name="typeOfSorting"
-        >
-          <option value="a-z">A-Z</option>
-          <option value="z-a">Z-A</option>
-          <option value="low-high">Low to High</option>
-          <option value="high-low">High to Low</option>
-        </select>
-      </StyledInputWrap>
-
-      <StyledInputWrap>
-        <label htmlFor="Shipping">free shipping?</label>
-        <input
-          value={newFreeShipping}
-          onChange={() => setNewFreeShipping((prev) => !prev)}
-          type="checkbox"
-          id="Shipping"
-          name="Shipping"
-        />
-      </StyledInputWrap>
-
-      <button
-        className=" w-[150px] h-[40px] bg-zinc-400 text-white rounded-lg text-[18px]"
-        onClick={(e) => {
-          e.preventDefault()
-
-          filterProducts(
-            searchTerm,
-            newPrice,
-            newCategory,
-            newCompany,
-            newSorting,
-            newFreeShipping
-          )
-        }}
-      >
-        search
-      </button>
+        <StyledInputWrap className=" w-[200px] flex flex-col text-start items-start justify-center">
+          <label htmlFor="searchTerm">search</label>
+          <input
+            className="text-black"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            type="text"
+            id="searchTerm"
+            name="searchTerm"
+          />
+        </StyledInputWrap>
+      </div>
     </form>
   )
 }
