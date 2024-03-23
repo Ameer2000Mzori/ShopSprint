20401155b96fde14d15659fa2544d7d9fb675e4a
Ameer Ameen <ameerapex496@gmail.com>
2024-03-24 00:32:23 +0100

    added schema for the user

--- backend/src/modules/module.js
+++ backend/src/modules/module.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+import mongoose, { Schema } from 'mongoose'
+
+const userSchema = new Schema({
+  userName: { type: String, required: true },
+  Name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  orderList: [
+    {
+      product: { type: Schema.Types.ObjectId, ref: 'Product' },
+      quantity: { type: Number, required: true },
+    },
+  ],
+})
+
+const User = mongoose.model('User', userSchema)
+
+export default User
