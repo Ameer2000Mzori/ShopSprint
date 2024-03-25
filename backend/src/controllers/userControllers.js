cfa66c380eb3a20b9d49aa7bd6058767ef5ebbc2
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-25 23:00:09 +0100

    added more logic to userControllers file

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -2,7 +2,7 @@ import { User } from '../modules/userSchema.js'
 
 export const getUsers = (req, res) => {
   User.find()
-    .populate('articles')
+    .populate('orderList')
     .sort({
       createdAt: -1,
     })
@@ -16,14 +16,37 @@ export const getUsers = (req, res) => {
     })
 }
 
-export const createUser = (req, res) => {
+export const createUser = async (req, res) => {
   const { username, email, password } = req.body
 
-  if (!username || !email || !password) {
-    return res
-      .status(400)
-      .json({ error: 'Missing required fields in request body' })
+  try {
+    const user = await User.findOne({
+      $or: [{ username }, { email }],
+    })
+    if (user)
+      return res.status(400).json({
+        message: 'user already exist',
+      })
+    // let new_pwd = await hashPassword(password)
+    newAccount = new User({
+      username,
+      email,
+      password: password,
+    })
+    await newAccount.save()
+    console.log(newAccount)
+    // const token = jwt.sign({ id: newAccount._id }, process.env.SECRET, {
+    //   expiresIn: 3600,
+    // }) // 1 hour
+    res.status(200).json({
+      message: 'user created successfully',
+      User: newAccount,
+      // token,
+    })
+  } catch {
+    // if (newAccount?._id) await newAccount.deleteOne({ _id: newAccount._id })
+    res.status(500).json({
+      message: 'Server failed',
+    })
   }
-
-  console.log('Received data for creating user:', username, email, password)
 }
