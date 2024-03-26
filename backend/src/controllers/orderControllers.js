5a14a4746054507ac384000178039619f2e86726
Ameer Ameen <ameerapex496@gmail.com>
2024-03-26 03:51:50 +0100

    added some logic for user to add an order

--- backend/src/controllers/orderControllers.js
+++ backend/src/controllers/orderControllers.js
@@ -15,10 +15,38 @@ export const getOrders = (req, res) => {
     })
 }
 
-export const getUserOrder = (req, res) => {
-  const { usrId } = req.body
-}
+export const getUserOrder = async (req, res) => {}
 
-export const addOrder = () => {
-  console.log('addOrder')
+export const addOrder = async (req, res) => {
+  const { id, name, price } = req.body
+  console.log(req.user)
+
+  try {
+    // finding user by id
+    const user = await User.findById(req.user.id)
+
+    // Create a new article
+    const newArticle = new Article({
+      author: user.id,
+      title,
+      text,
+    })
+
+    // Save the new article
+    await newArticle.save()
+
+    // Push the newly created article to the user's articles array
+    user.articles.push(newArticle)
+
+    //  update the user
+    await user.save()
+
+    res.status(200).json({
+      message: 'Article created successfully',
+      article: newArticle,
+    })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ error: 'An error occurred' })
+  }
 }
