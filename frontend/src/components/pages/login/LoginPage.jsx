e373c329784e86b69d461e44ce7580e7b3a82ae5
Ameer Ameen <ameerapex496@gmail.com>
2024-04-13 00:53:33 +0200

    added styled component and more to the project

--- frontend/src/components/pages/login/LoginPage.jsx
+++ frontend/src/components/pages/login/LoginPage.jsx
@@ -1,5 +1,45 @@
 import React from 'react'
+import {
+  StyledForm,
+  StyledFormWrap,
+  StyledLabelInput,
+  StyledLabelInputWrap,
+} from './component/StyledComponent.jsx'
 
-export const LoginPage = () => {
-  return <div>LoginPage</div>
+const LoginPage = () => {
+  return (
+    <StyledFormWrap>
+      <StyledForm>
+        <StyledLabelInputWrap>
+          <StyledLabelInput htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </StyledLabelInput>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+        </StyledLabelInputWrap>
+        <StyledLabelInputWrap>
+          <StyledLabelInput
+            htmlFor="exampleInputPassword1"
+            className="form-label"
+          >
+            Password
+          </StyledLabelInput>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </StyledLabelInputWrap>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </StyledForm>
+    </StyledFormWrap>
+  )
 }
+
+export default LoginPage
---
+++ frontend/src/components/pages/login/component/StyledComponent.jsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export const StyledFormWrap = styled.form`
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  gap: 5px;
+`
+export const StyledForm = styled.form`
+  background-color: #021431;
+  height: 175px;
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+`
+
+export const StyledLabelInputWrap = styled.form`
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  justify-content: center;
+  width: 250px;
+`
+
+export const StyledLabelInput = styled.label`
+  color: #fff;
+  font-size: 20px;
+`
