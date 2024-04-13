bd32216b07f5039b0d2dd0805679c285d1168ca0
Ameer Ameen <ameerapex496@gmail.com>
2024-04-13 12:52:31 +0200

    added some styling and login page

--- frontend/src/components/pages/navbar/component/UtilityBar.jsx
+++ frontend/src/components/pages/navbar/component/UtilityBar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const UtilityBar = () => {
   return (
     <div className="h-[40px] w-[100%] flex flex-col text-center items-center justify-center bg-[#021431] text-white">
       <div className="w-[65%] flex flex-row text-center items-end justify-end gap-4 pr-8">
-        <button className="hover:underline">Sign in / Guest</button>
+        <Link to="/Login" className="hover:underline">
+          Sign in
+        </Link>
         <button className="hover:underline">Create Account</button>
       </div>
     </div>
