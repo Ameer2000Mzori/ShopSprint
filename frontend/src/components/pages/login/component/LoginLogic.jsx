c8e2f00c4b06500b5907eb4063ed6be2f4071e22
Ameer Ameen <ameerapex496@gmail.com>
2024-04-14 20:46:55 +0200

    putting this project to rest for now

--- frontend/src/components/pages/login/component/LoginLogic.jsx
+++ frontend/src/components/pages/login/component/LoginLogic.jsx
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
-import { updateUser } from '../../../features/user/userSlice'
 
 const LoginLogic = async (email, password) => {
   const dispatch = useDispatch()
@@ -8,7 +7,6 @@ const LoginLogic = async (email, password) => {
     const response = await axios.post('/login', { email, password })
 
     if (response.status >= 200 && response.status < 300) {
-      dispatch(updateUser(response.data))
       return response.data
     } else {
       console.error('Login failed:', response.statusText)
