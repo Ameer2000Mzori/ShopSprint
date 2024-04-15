7f542f15524213d4a09bbaa7f3dcb54c68b371ee
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-04-15 21:31:00 +0200

    fixed login function

--- frontend/src/components/pages/login/component/LoginLogic.jsx
+++ frontend/src/components/pages/login/component/LoginLogic.jsx
@@ -1,17 +1,12 @@
 import axios from 'axios'
-import { useDispatch } from 'react-redux'
+// import { useDispatch } from 'react-redux'
 
 const LoginLogic = async (email, password) => {
-  const dispatch = useDispatch()
+  // const dispatch = useDispatch()
   try {
     const response = await axios.post('/login', { email, password })
 
-    if (response.status >= 200 && response.status < 300) {
-      return response.data
-    } else {
-      console.error('Login failed:', response.statusText)
-      return { success: false, message: 'Login failed' }
-    }
+    return response.data
   } catch (error) {
     console.error('Login failed:', error)
     return { success: false, message: 'Login failed' }
