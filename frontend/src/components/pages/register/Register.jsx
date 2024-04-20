cc0f24131dfa706b9d58207e96cf00b7eec739d7
Ameer Ameen <ameerapex496@gmail.com>
2024-04-21 00:20:46 +0200

    used states

--- frontend/src/components/pages/register/Register.jsx
+++ frontend/src/components/pages/register/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   StyledButton,
   StyledForm,
@@ -8,19 +8,40 @@ import {
 } from '../login/component/StyledComponent'
 
 const Register = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   return (
     <StyledFormWrap>
       <StyledForm>
         <StyledLabelInputWrap>
-          <StyledLabelInput htmlFor="name">Name</StyledLabelInput>
+          <StyledLabelInput
+            htmlFor="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          >
+            Name
+          </StyledLabelInput>
           <input type="text" id="name" />
         </StyledLabelInputWrap>
         <StyledLabelInputWrap>
-          <StyledLabelInput htmlFor="email">Email address</StyledLabelInput>
+          <StyledLabelInput
+            htmlFor="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          >
+            Email address
+          </StyledLabelInput>
           <input type="text" id="email" />
         </StyledLabelInputWrap>
         <StyledLabelInputWrap>
-          <StyledLabelInput htmlFor="password">Password</StyledLabelInput>
+          <StyledLabelInput
+            htmlFor="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          >
+            Password
+          </StyledLabelInput>
           <input type="password" id="password" />
         </StyledLabelInputWrap>
         <StyledButton type="submit">Submit</StyledButton>
