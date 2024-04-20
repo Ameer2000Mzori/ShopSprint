0743647208d937c5c008d8c52803979e2f5a32b0
Ameer Ameen <ameerapex496@gmail.com>
2024-04-20 23:05:18 +0200

    fixed few things

--- frontend/src/components/pages/login/component/LoginLogic.jsx
+++ frontend/src/components/pages/login/component/LoginLogic.jsx
@@ -1,8 +1,6 @@
 import axios from 'axios'
-// import { useDispatch } from 'react-redux'
 
 const LoginLogic = async (email, password) => {
-  // const dispatch = useDispatch()
   try {
     const response = await axios.post('/login', { email, password })
 
