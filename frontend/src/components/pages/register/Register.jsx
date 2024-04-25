e79d86ea547907693041ad403788545814660e3b
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-04-25 03:35:27 +0200

    added formik and yup

--- frontend/src/components/pages/register/Register.jsx
+++ frontend/src/components/pages/register/Register.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useFormik } from 'formik'
 import {
   StyledButton,
   StyledForm,
@@ -9,28 +10,44 @@ import {
 import AuthOperations from '../../shared/AuthOperations'
 import useStoreToken from '../../shared/useStoreToken'
 import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { validationSchemaRegister } from '../../shared/validationSchema'
 
 const Register = () => {
-  const [name, setName] = useState('')
-  const [userName, setUserName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
   const saveData = useStoreToken()
-  const navigate = useNavigate()
+  const token = useSelector((state) => state.user.token)
   const { mutate, isPending, isSuccess, isError, data } = AuthOperations()
-  const handleSubmit = () => {
-    console.log('this data got', name, email, password)
-    mutate([
-      {
-        method: 'POST',
-        url: 'register',
-        name: name,
-        username: userName,
-        email: email,
-        password: password,
-      },
-    ])
-  }
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      userName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+    onSubmit: (values) => {
+      console.log('values onsubmit register', values)
+
+      mutate([
+        {
+          method: 'POST',
+          url: 'register',
+          name: values.name,
+          username: values.userName,
+          email: values.email,
+          password: values.password,
+        },
+      ])
+    },
+    validationSchema: validationSchemaRegister,
+  })
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (token) navigate('/')
+  }, [token])
 
   if (isPending) return <div>isPending...</div>
 
@@ -44,47 +61,73 @@ const Register = () => {
 
   return (
     <StyledFormWrap>
-      <StyledForm
-        onSubmit={(e) => {
-          e.preventDefault()
-          handleSubmit(name, email, password)
-        }}
-      >
+      <StyledForm onSubmit={formik.handleSubmit}>
         <StyledLabelInputWrap>
           <StyledLabelInput htmlFor="name">Name</StyledLabelInput>
           <input
             type="text"
             id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            onChange={formik.handleChange}
+            value={formik.values.name}
+            onBlur={formik.handleBlur}
           />
+          {formik.touched.name && formik.errors.name ? (
+            <p className="text-white">{formik.errors.name}</p>
+          ) : null}
         </StyledLabelInputWrap>
         <StyledLabelInputWrap>
-          <StyledLabelInput htmlFor="username">User Name</StyledLabelInput>
+          <StyledLabelInput htmlFor="userName">User Name</StyledLabelInput>
           <input
             type="text"
-            id="username"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
+            id="userName"
+            onChange={formik.handleChange}
+            value={formik.values.userName}
+            onBlur={formik.handleBlur}
           />
+          {formik.touched.userName && formik.errors.userName && (
+            <p className="text-white">{formik.errors.userName}</p>
+          )}
         </StyledLabelInputWrap>
         <StyledLabelInputWrap>
           <StyledLabelInput htmlFor="email">Email address</StyledLabelInput>
           <input
             type="text"
             id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={formik.handleChange}
+            value={formik.values.email}
+            onBlur={formik.handleBlur}
           />
+          {formik.touched.email && formik.errors.email ? (
+            <p className="text-white">{formik.errors.email}</p>
+          ) : null}
         </StyledLabelInputWrap>
         <StyledLabelInputWrap>
           <StyledLabelInput htmlFor="password">Password</StyledLabelInput>
           <input
             type="password"
             id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={formik.handleChange}
+            value={formik.values.password}
+            onBlur={formik.handleBlur}
           />
+          {formik.touched.password && formik.errors.password ? (
+            <p className="text-white">{formik.errors.password}</p>
+          ) : null}
+        </StyledLabelInputWrap>
+        <StyledLabelInputWrap>
+          <StyledLabelInput htmlFor="confirmPassword">
+            Confirm Password
+          </StyledLabelInput>
+          <input
+            type="password"
+            id="confirmPassword"
+            onChange={formik.handleChange}
+            value={formik.values.confirmPassword}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.confirmPassword && formik.errors.confirmPassword && (
+            <p className="text-white">{formik.errors.confirmPassword}</p>
+          )}
         </StyledLabelInputWrap>
         {isError?.response?.data?.message && (
           <div className="text-white">{isError?.response?.data?.message}</div>
--- frontend/src/components/shared/validationSchema.js
+++ frontend/src/components/shared/validationSchema.js
@@ -1,4 +1,14 @@
-// import * as Yup from 'yup'
+import * as Yup from 'yup'
+
+export const validationSchemaRegister = Yup.object().shape({
+  name: Yup.string().required('name is required'),
+  userName: Yup.string().required(' userName is required'),
+  email: Yup.string().required('Email is required'),
+  password: Yup.string().required('Password is required'),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref('password'), null], 'Passwords must match')
+    .required('Confirm Password is required'),
+})
 
 // export const validationSchemaLogin = Yup.object().shape({
 //   email: Yup.string().required('Email is required'),
