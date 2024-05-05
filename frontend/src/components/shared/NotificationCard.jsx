03de98b5395c5c15e9d989f830514c8904942f9a
Ameer Ameen <ameerapex496@gmail.com>
2024-05-05 06:10:13 +0200

    added new file for notifications

---
+++ frontend/src/components/shared/NotificationCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { toast } from 'react-toastify'
+const NotificationCard = ({ option, message }) => {
+  switch (option) {
+    case 'success':
+      toast.success(message)
+      break
+    case 'error':
+      toast.error(message)
+      break
+    case 'info':
+      toast.info(message)
+      break
+    case 'warning':
+      toast.warning(message)
+      break
+    default:
+      toast.info(message)
+      break
+  }
+}
+
+export default NotificationCard
