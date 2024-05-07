2bd2b794a645cb55366a6f5ebc3d34d69a0833df
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-05-08 01:29:34 +0200

    added changes and new ui component card

--- frontend/src/components/pages/home/Home.jsx
+++ frontend/src/components/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Header from './component/Header.jsx'
 import FeaturedProducts from './component/FeaturedProducts.jsx'
 import backgroundImage from '../../../assets/bg-img.jpg'
+import { Button, Heading, Text } from '@chakra-ui/react'
+
 const Home = () => {
   return (
     <div
@@ -17,6 +19,11 @@ const Home = () => {
       }}
       className="w-[100%] p-12 overflow-hidden flex flex-col items-center justify-center "
     >
+      <Heading size="lg">Welcome to Chakra UI!</Heading>
+      <Text fontSize="xl">
+        Start building your awesome app with Chakra components.
+      </Text>
+      <Button colorScheme="blue">Get Started</Button>
       <div className="w-[65%] h-auto pb-4 flex flex-col items-center justify-center">
         <Header />
         <FeaturedProducts />
--- frontend/src/components/shared/ProductCard.jsx
+++ frontend/src/components/shared/ProductCard.jsx
@@ -1,23 +1,61 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { StyledCard } from '../shared/StyledComponents.jsx'
+import {
+  Card,
+  Text,
+  Button,
+  Divider,
+  ButtonGroup,
+  Heading,
+  Stack,
+  Image,
+  CardBody,
+  CardFooter,
+} from '@chakra-ui/react'
 
 const ProductCard = ({ item }) => {
   return (
     <>
       <Link key={item.id} to={`/product/${item.id}`}>
-        <StyledCard className="p-4">
-          <img
-            className="h-[150px] w-[350px] bg-white rounded-lg object-cover"
-            src="https://via.placeholder.com/300x200"
-            alt=""
-          />
-          <div className="flex flex-row flex-wrap text-center items-center justify-center gap-4">
-            <h1> name : {item.title}</h1>
-            <h1> price : ${item.price}</h1>
-            <p>free shipping ? : {item.freeShipping ? 'yes' : 'no'}</p>
-          </div>
-        </StyledCard>
+        <Card key={item.id} maxW="sm">
+          <CardBody
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              textAlign: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Image
+              src="https://via.placeholder.com/300x200"
+              alt="Green double couch with wooden legs"
+              borderRadius="lg"
+            />
+            <Stack mt="6" spacing="3">
+              <Heading size="md">{item.title}</Heading>
+              <Text>
+                This sofa is perfect for modern tropical spaces, baroque
+                inspired spaces, earthy toned spaces and for people who love a
+                chic design with a sprinkle of vintage design.
+              </Text>
+              <Text color="blue.600" fontSize="2xl">
+                ${item.price}
+              </Text>
+              <Text color="blue.600" className="text-start text-[15px]">
+                free shipping - {item.freeShipping ? 'yes' : 'no'}
+              </Text>
+            </Stack>
+          </CardBody>
+          <Divider />
+          <CardFooter>
+            <ButtonGroup spacing="2">
+              <Button variant="solid" colorScheme="blue">
+                Add to cart
+              </Button>
+            </ButtonGroup>
+          </CardFooter>
+        </Card>
       </Link>
     </>
   )
