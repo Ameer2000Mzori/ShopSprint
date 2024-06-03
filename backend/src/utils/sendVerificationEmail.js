4293b377e78438eed457c3179ae608047fb582c5
Ameer Ameen <ameerapex496@gmail.com>
2024-06-03 23:30:27 +0200

    added more logic

--- backend/src/utils/sendVerificationEmail.js
+++ backend/src/utils/sendVerificationEmail.js
@@ -1,2 +1,13 @@
 import { Resend } from 'resend'
-const resend = new Resend('re_123456789')
+import 'dotenv/config'
+
+const resend = new Resend(`${process.env.RESEND_ID}`)
+
+export default async function sendVerificationEmail({ userEmail, Text }) {
+  await resend.emails.send({
+    from: '<ameerapex496@gmail.com>',
+    to: [`${userEmail}`],
+    subject: 'account verification',
+    text: `PLEASE CLICK HERE FOR ACCOUNT ${Text} `,
+  })
+}
