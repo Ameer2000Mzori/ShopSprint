2b766d069b1492ab3708fe8d240aad2f57625cf2
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-06-12 23:14:14 +0200

    added few changes

--- frontend/src/components/pages/userdashboard/userDashBoard.jsx
+++ frontend/src/components/pages/userdashboard/userDashBoard.jsx
@@ -1,5 +1,20 @@
 import React from 'react'
 
 export default function userDashBoard() {
-  return <div>userDashBoard</div>
+  return (
+    <div>
+      <div>
+        <ul>
+          <li>profile</li>
+          <li>orders</li>
+          <li>settings</li>
+          <li>logout</li>
+          <li>admin / if hes</li>
+        </ul>
+      </div>
+      <div>
+        <h1>here is the selected page</h1>
+      </div>
+    </div>
+  )
 }
