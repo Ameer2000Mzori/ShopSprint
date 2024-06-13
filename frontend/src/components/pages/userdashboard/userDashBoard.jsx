1846a25e4279ac39e2b4e71c2dfad57231dc193c
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-06-13 22:24:23 +0200

    added styling to the user dashboard list

--- frontend/src/components/pages/userdashboard/userDashBoard.jsx
+++ frontend/src/components/pages/userdashboard/userDashBoard.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function userDashBoard() {
   return (
-    <div>
-      <div>
-        <ul>
+    <div className="h-[100%] flex flex-row text-center items-start justify-start">
+      <div className="flex flex-col text-center items-center justify-center p-[5px]">
+        <ul className="flex flex-col text-start items-start justify-start gap-[5px] w-[100%] h-[100%]">
           <li>profile</li>
           <li>orders</li>
           <li>settings</li>
